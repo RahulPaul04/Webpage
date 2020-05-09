4f23363c111e4ee9eabc27ca4fcfed0160b4096c
unknown <rahulpaul0498@gmail.com>
2020-05-09 16:30:31 +0530

    AjAX-15

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -101,8 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-      var category_name = chooseRandomCategory(categories).short_name;
-      category_name = string(category_name)
+      var category_name = 'chooseRandomCategory(categories).short_name;'
       console.log(category_name);
       var chosenCategoryShortName = category_name;
 
