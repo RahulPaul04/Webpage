948a804b555f656fb92145b189f5f312c5a11700
unknown <rahulpaul0498@gmail.com>
2020-05-09 15:45:16 +0530

    AjAX-7

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -101,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-      var chosenCategoryShortName = chooseRandomCategory(categories)
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -141,6 +141,7 @@ function chooseRandomCategory (categories) {
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
 
   // return category object with that randomArrayIndex
+  console.log(categories[randomArrayIndex]);
   return categories[randomArrayIndex];
 }
 
