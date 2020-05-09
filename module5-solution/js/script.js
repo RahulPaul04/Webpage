790bcd1513cb8d5fc739b5289cad68f373f4ec48
unknown <rahulpaul0498@gmail.com>
2020-05-09 17:09:45 +0530

    AjAX-23

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -119,9 +119,10 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       console.log("hello");
+      console.log(chosenCategoryShortName);
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
                      "randomCategoryShortName",
-                     {chosenCategoryShortName});
+                     {{chosenCategoryShortName}});
 
 
 
